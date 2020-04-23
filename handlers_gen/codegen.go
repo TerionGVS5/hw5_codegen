f7a96395094d0b1e2958bbb23a6e8d3492f1af15
vladimir <goncharukvs5@gmail.com>
2020-04-23 11:48:01 +0300

    fill common response

--- handlers_gen/codegen.go
+++ handlers_gen/codegen.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"go/ast"
 	"go/parser"
@@ -9,6 +10,12 @@ import (
 	"os"
 	"strings"
 )
+
+type ApiGen struct {
+	Url    string `json:"url"`
+	Auth   bool   `json:"auth"`
+	Method string `json:"method"`
+}
 
 func main() {
 	fset := token.NewFileSet()
@@ -21,11 +28,19 @@ func main() {
 
 	fmt.Fprintln(out, `package `+node.Name.Name)
 	fmt.Fprintln(out) // empty line
-	fmt.Fprintln(out, `import _ "encoding/json"`)
+	fmt.Fprintln(out, `import "encoding/json"`)
 	fmt.Fprintln(out, `import _ "net/http"`)
 	fmt.Fprintln(out, `import _ "reflect"`)
 	fmt.Fprintln(out, `import _ "strconv"`)
 	fmt.Fprintln(out) // empty line
+
+	// fill common error responses
+	fmt.Fprintln(out, `var unknownMethodResponse, _ = json.Marshal(map[string]string{
+	"error": "unknown method",
+	})`)
+	fmt.Fprintln(out) // empty line
+	addedBadMethodResponse := false
+	addedUnauthorizedResponse := false
 
 	for _, decl := range node.Decls {
 		funcDecl, ok := decl.(*ast.FuncDecl)
@@ -37,6 +52,24 @@ func main() {
 				if !strings.HasPrefix(docString.Text, "// apigen:api ") {
 					continue
 				}
+				var currApiGen ApiGen
+				// add common error response if need
+				json.Unmarshal([]byte(strings.Replace(docString.Text, "// apigen:api ", "", 1)), &currApiGen)
+				if !addedBadMethodResponse && currApiGen.Method != "" {
+					addedBadMethodResponse = true
+					fmt.Fprintln(out, `var badMethodResponse, _ = json.Marshal(map[string]string{
+						"error": "bad method",
+					})`)
+					fmt.Fprintln(out) // empty line
+				}
+				if !addedUnauthorizedResponse && currApiGen.Auth == true {
+					addedUnauthorizedResponse = true
+					fmt.Fprintln(out, `var unauthorizedResponse, _ = json.Marshal(map[string]string{
+						"error": "unauthorized",
+					})`)
+					fmt.Fprintln(out) // empty line
+				}
+				// here fill handler
 				for _, funcParam := range funcDecl.Type.Params.List {
 					if funcParam.Names[0].Name != "in" {
 						continue
